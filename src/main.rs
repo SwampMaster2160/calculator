e4aecc0995219758f2951423a6d6c8663de1457d
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2022-05-24 16:37:54 +1200

    9

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,27 @@ fn run_fn(params: Vec<StackToken>, func_name: &String) -> StackToken
 		"acosh" => 1,
 		"atanh" => 1,
 		"root" => 2,
-		"neg" => 1,
+		"floor" => 1,
+		"ceil" => 1,
+		"round" => 1,
+		"abs" => 1,
+		"sgn" => 1,
+		"exp" => 1,
+		"exp2" => 1,
+		"ln" => 1,
+		"log" => 2,
+		"log2" => 1,
+		"log10" => 1,
+		"hypot" => 2,
+		"atan2" => 2,
+		"exp_m1" => 1,
+		"ln_1p" => 1,
+		"recip" => 1,
+		"to_degrees" => 1,
+		"to_radians" => 1,
+		"max" => 2,
+		"min" => 2,
+		"clamp" => 3,
 		_ => 0
 	};
 	if param_count == 0
@@ -60,37 +80,87 @@ fn run_fn(params: Vec<StackToken>, func_name: &String) -> StackToken
 		println!("Error: Invalid parameter count.");
 		std::process::exit(0);
 	}
-	return match func_name.as_str()
+	return StackToken::Number(match func_name.as_str()
 	{
-		"sqrt" => StackToken::Number(nums[0].sqrt()),
-		"cbrt" => StackToken::Number(nums[0].cbrt()),
-		"sin" => StackToken::Number(nums[0].sin()),
-		"cos" => StackToken::Number(nums[0].cos()),
-		"tan" => StackToken::Number(nums[0].tan()),
-		"asin" => StackToken::Number(nums[0].asin()),
-		"acos" => StackToken::Number(nums[0].acos()),
-		"atan" => StackToken::Number(nums[0].atan()),
-		"sinh" => StackToken::Number(nums[0].sinh()),
-		"cosh" => StackToken::Number(nums[0].cosh()),
-		"tanh" => StackToken::Number(nums[0].tanh()),
-		"asinh" => StackToken::Number(nums[0].asinh()),
-		"acosh" => StackToken::Number(nums[0].acosh()),
-		"atanh" => StackToken::Number(nums[0].atanh()),
-		"root" => StackToken::Number(nums[0].powf(1f64 / nums[1])),
-		"neg" => StackToken::Number(-nums[0]),
+		"sqrt" => nums[0].sqrt(),
+		"cbrt" => nums[0].cbrt(),
+		"sin" => nums[0].sin(),
+		"cos" => nums[0].cos(),
+		"tan" => nums[0].tan(),
+		"asin" => nums[0].asin(),
+		"acos" => nums[0].acos(),
+		"atan" => nums[0].atan(),
+		"sinh" => nums[0].sinh(),
+		"cosh" => nums[0].cosh(),
+		"tanh" => nums[0].tanh(),
+		"asinh" => nums[0].asinh(),
+		"acosh" => nums[0].acosh(),
+		"atanh" => nums[0].atanh(),
+		"root" => nums[0].powf(1f64 / nums[1]),
+		"floor" => nums[0].floor(),
+		"ceil" => nums[0].ceil(),
+		"round" => nums[0].round(),
+		"abs" => nums[0].abs(),
+		"sgn" => nums[0].signum(),
+		"exp" => nums[0].exp(),
+		"exp2" => nums[0].exp2(),
+		"ln" => nums[0].ln(),
+		"log" => nums[0].log(nums[1]),
+		"log2" => nums[0].log2(),
+		"log10" => nums[0].log10(),
+		"hypot" => nums[0].hypot(nums[1]),
+		"atan2" => nums[0].atan2(nums[1]),
+		"exp_m1" => nums[0].exp_m1(),
+		"ln_1p" => nums[0].ln_1p(),
+		"recip" => nums[0].recip(),
+		"to_degrees" => nums[0].to_degrees(),
+		"to_radians" => nums[0].to_radians(),
+		"max" => nums[0].max(nums[1]),
+		"min" => nums[0].min(nums[1]),
+		"clamp" => nums[0].clamp(nums[1], nums[2]),
 		_ =>
 		{
 			println!("Error: Invalid function.");
 			std::process::exit(0);
 		}
-	}
+	});
 }
 
 fn solve(mut to_solve: Vec<StackToken>) -> StackToken
 {
+	// Handle the negitive sign
+	let mut x: usize = 0;
+	while x < to_solve.len()
+	{
+		if matches!(to_solve[x], StackToken::Operator('-')) && (x == 0 || !matches!(to_solve[x - 1], StackToken::Number(_)))
+		{
+			let val_1 = match to_solve.get(x + 1)
+			{
+				Some(StackToken::Number(valid)) => *valid,
+				Some(_) =>
+				{
+					println!("Error: There is an operator that does not have a number to its right.");
+					std::process::exit(0);
+				},
+				None =>
+				{
+					println!("Error: Operator at end of expression.");
+					std::process::exit(0);
+				}
+			};
+			to_solve.remove(x + 1);
+			to_solve[x] = StackToken::Number(-val_1);
+		}
+		else
+		{
+			x += 1;
+		}
+	}
+
+	// Other operators
 	for operations in OPERATIONS
 	{
-		let mut x: usize = 0;
+		x = 0;
 		while x < to_solve.len()
 		{
 			match to_solve[x]
@@ -115,12 +185,6 @@ fn solve(mut to_solve: Vec<StackToken>) -> StackToken
 						};
 						if x == 0
 						{
-							if chr == '-'
-							{
-								to_solve.remove(x + 1);
-								to_solve[x] = StackToken::Number(-val_1);
-								continue;
-							}
 							println!("Error: Operator at start of expression.");
 							std::process::exit(0);
 						}
@@ -129,12 +193,6 @@ fn solve(mut to_solve: Vec<StackToken>) -> StackToken
 							Some(StackToken::Number(valid)) => *valid,
 							Some(_) =>
 							{
-								if chr == '-'
-								{
-									to_solve.remove(x);
-									to_solve[x - 1] = StackToken::Number(-val_1);
-									continue;
-								}
 								println!("Error: There is an operator that does not have a number to its left.");
 								std::process::exit(0);
 							},
@@ -184,102 +242,101 @@ fn main()
 		return;
 	}
 
-	// For each char in second argument
+	// Init
 	let eq: Vec<char> = args[1].chars().collect();
 	let mut parse_mode = ParseMode::None;
 	let mut stack: Vec<Vec<Vec<StackToken>>> = vec![vec![Vec::new()]];
+
+	// For each char in second argument
 	for (x, chr) in eq.iter().enumerate()
 	{
-		// Parse char
-		match parse_mode
+		// If we are not already parsing something
+		if matches!(parse_mode, ParseMode::None)
 		{
-			ParseMode::None =>
+			// If we come accross a number
+			if chr.is_numeric() || *chr == '.'
 			{
-				if chr.is_numeric() || *chr == '.'
+				parse_mode = ParseMode::Numeric(x);
+			}
+			// If we come accross a keyword
+			else if chr.is_alphabetic() || *chr == '_'
+			{
+				parse_mode = ParseMode::Alphanumeric(x);
+			}
+			// Parameter seperator
+			else if *chr == ','
+			{
+				stack.last_mut().unwrap().push(Vec::new());
+			}
+			// Start enclose
+			else if *chr == '('
+			{
+				stack.push(vec![Vec::new()]);
+			}
+			// End enclose
+			else if *chr == ')'
+			{
+				let mut solved: Vec<StackToken> = Vec::new();
+				match stack.pop()
 				{
-					parse_mode = ParseMode::Numeric(x);
-				}
-				else if chr.is_alphabetic() || *chr == '_'
-				{
-					parse_mode = ParseMode::Alphanumeric(x);
-				}
-				else if !chr.is_whitespace()
-				{
-					if *chr == ','
+					Some(valid) =>
 					{
-						stack.last_mut().unwrap().push(Vec::new());
-					}
-					else if *chr == '('
-					{
-						stack.push(vec![Vec::new()]);
-					}
-					else if *chr == ')'
-					{
-						let mut solved: Vec<StackToken> = Vec::new();
-						match stack.pop()
+						for sub_exp in valid
 						{
-							Some(valid) =>
-							{
-								//top = solve(valid);
-								//let mut solved: Vec<StackToken> = Vec::new();
-								for sub_exp in valid
-								{
-									solved.push(solve(sub_exp));
-								}
-							}
-							None =>
-							{
-								println!("Error: More closing brackets than opening brackets.");
-								return;
-							}
-						}
-						let top;
-						match stack.last_mut()
-						{
-							Some(valid) =>
-							{
-								match valid.last().unwrap().last()
-								{
-									Some(StackToken::Keyword(keyword)) =>
-									{
-										top = run_fn(solved, &keyword);
-										valid.last_mut().unwrap().pop();
-									},
-									Some(_) =>
-									{
-										if solved.len() != 1
-										{
-											println!("Error: Only allowed one result.");
-											return;
-										}
-										top = solved.pop().unwrap();
-									},
-									None =>
-									{
-										if solved.len() != 1
-										{
-											println!("Error: Only allowed one result.");
-											return;
-										}
-										top = solved.pop().unwrap();
-									}
-								}
-								valid.last_mut().unwrap().push(top);
-							}
-							None =>
-							{
-								println!("Error: More closing brackets than opening brackets.");
-								return;
-							}
+							solved.push(solve(sub_exp));
 						}
 					}
-					else
+					None =>
 					{
-						stack.last_mut().unwrap().last_mut().unwrap().push(StackToken::Operator(*chr));
+						println!("Error: More closing brackets than opening brackets.");
+						return;
 					}
 				}
-			},
-			_ => {}
+				let top;
+				match stack.last_mut()
+				{
+					Some(valid) =>
+					{
+						match valid.last().unwrap().last()
+						{
+							Some(StackToken::Keyword(keyword)) =>
+							{
+								top = run_fn(solved, &keyword);
+								valid.last_mut().unwrap().pop();
+							},
+							Some(_) =>
+							{
+								if solved.len() != 1
+								{
+									println!("Error: Only allowed one result.");
+									return;
+								}
+								top = solved.pop().unwrap();
+							},
+							None =>
+							{
+								if solved.len() != 1
+								{
+									println!("Error: Only allowed one result.");
+									return;
+								}
+								top = solved.pop().unwrap();
+							}
+						}
+						valid.last_mut().unwrap().push(top);
+					}
+					None =>
+					{
+						println!("Error: More closing brackets than opening brackets.");
+						return;
+					}
+				}
+			}
+			// Operator
+			else if !chr.is_whitespace()
+			{
+				stack.last_mut().unwrap().last_mut().unwrap().push(StackToken::Operator(*chr));
+			}
 		}
 
 		// Get next char
@@ -331,8 +388,7 @@ fn main()
 		println!("Error: Only allowed one result.");
 		return;
 	}
-	let top = top.pop().unwrap();
-	let last_solved = solve(top);
+	let last_solved = solve(top.pop().unwrap());
 
 	// Print result
 	match last_solved
